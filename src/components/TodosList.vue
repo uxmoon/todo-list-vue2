aa6761319302424c9569ed1a132138daf31eba1b
Martin Luna <maluna@technisys.com>
2022-03-23 14:39:29 -0300

    Import item component

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <h1>Todos List</h1>
-    <ul v-bind:key="todo.id" v-for="todo in todos">
-      <li>{{todo.title}}</li>
-    </ul>
-  </div>  
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodosItem v-bind:todo="todo" />
+    </div>
+  </div>
 </template>
 
 <script>
+import TodosItem from "./TodosItem.vue";
+
 export default {
   name: "TodosList",
-  props: ["todos"]
-}
+  components: { TodosItem },
+  props: ["todos"],
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
