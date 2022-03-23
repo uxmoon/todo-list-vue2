1cf561b4236b903edbb0080ca725212c61336a6c
Martin Luna <maluna@technisys.com>
2022-03-23 14:39:12 -0300

    Added prop and styles to item

--- src/components/TodosItem.vue
+++ src/components/TodosItem.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-    <p>item</p>
-  </div>
+  <p class="todo-item">{{ todo.title }}</p>
 </template>
 
 <script>
 export default {
   name: "TodosItem",
-}
+  props: ["todo"],
+};
 </script>
 
 <style scoped>
-
+.todo-item {
+  padding: 1rem;
+  background-color: #fff;
+  margin-bottom: 0.25rem;
+}
 </style>
