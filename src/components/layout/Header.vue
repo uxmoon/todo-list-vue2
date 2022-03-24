9267b2259d229d41fd32d348964ffa77167d214c
Martin Luna <maluna@technisys.com>
2022-03-24 17:16:24 -0300

    Updated global styles and indentation

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -4,30 +4,30 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </nav>    
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'HeaderApp'
-}
+  name: "HeaderApp",
+};
 </script>
 
 <style scoped>
-  .header {
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 1rem;
-  }
+.header {
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 1rem;
+}
 
-  .header a {
-    color: #fff;
-    text-decoration: none;
-  }
+.header a {
+  color: #fff;
+  text-decoration: none;
+}
 
-  .header a:hover {
-    text-decoration: underline;
-  }
+.header a:hover {
+  text-decoration: underline;
+}
 </style>
